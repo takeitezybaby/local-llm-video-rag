e3596446e964f77598d68d7208bed42c9f1ce6d6
Yash Chawla <yash336aarav@gmail.com>
2025-12-11 13:19:39 +0530

    testing code with sample file

--- SpeechToText.py
+++ SpeechToText.py
@@ -1,2 +1,14 @@
 import whisper
-model = whisper.load_model("large-v2")
+import json
+model = whisper.load_model("large-v2") 
+
+result = model.transcribe(audio="C:\\Users\\yash3\\Desktop\\RAG\\mp3files\\sample.mp3",
+                          language = "hi",
+                          task = "translate",
+                          word_timestamps=False)
+chunks = []
+for segment in result["segments"] :
+      chunks.append({"start" : segment["start"], "end" : segment["end"], "text" : segment["text"]})
+
+with open("output.json", "w") as f :
+      json.dump(chunks,f)
